18e2d31b1575339fb9bc8e2ad680fa2edfe040d3
FILLIN-RX <djeutchouruxel@gmail.com>
2025-11-11 14:48:04 +0100

    db setting

--- backend/index.js
+++ backend/index.js
@@ -99,7 +99,7 @@ app.use((req, res, next) => {
 });
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || ,
+    origin: process.env.FRONTEND_URL || 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
